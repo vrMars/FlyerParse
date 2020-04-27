21c968df1c95da5d1aa2da8f72012ee00ed2a7b3
Neelaksh Bhatia <vrmars98@gmail.com>
2020-04-26 21:01:14 -0400

    update script

--- dataset/getFlyers.py
+++ dataset/getFlyers.py
@@ -1,19 +1,14 @@
 import requests
 import json
 
-merchantList = [
-    'Freshco',
-    'Walmart',
-]
-
-with open('merchants.txt', 'r') as file:
+with open('./dataset/merchants.txt', 'r') as file:
     merchantList = file.read().split('\n')
 
     uri = 'https://backflipp.wishabi.com/flipp/items/search?locale=en-ca&postal_code=L6X2B4&q='
 
     for merchant in merchantList:
         resp = requests.get(uri + merchant).json()
-        f = open(merchant + '.json', 'w')
+        f = open('./dataset/' + merchant + '.json', 'w')
         f.write(json.dumps(resp))
         f.close()
 
