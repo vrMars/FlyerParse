7ee0b78aac0063b43d25be26340f708f07f12b3b
Neelaksh Bhatia <vrmars98@gmail.com>
2020-04-26 20:12:56 -0400

    deploy

--- dataset/getFlyers.py
+++ dataset/getFlyers.py
@@ -6,11 +6,15 @@ merchantList = [
     'Walmart',
 ]
 
-uri = 'https://backflipp.wishabi.com/flipp/items/search?locale=en-ca&postal_code=L6X2B4&q='
+with open('merchants.txt', 'r') as file:
+    merchantList = file.read().split('\n')
 
-for merchant in merchantList:
-    resp = requests.get(uri + merchant).json()
-    f = open(merchant + '.json', 'w')
-    f.write(json.dumps(resp))
-    f.close()
+    uri = 'https://backflipp.wishabi.com/flipp/items/search?locale=en-ca&postal_code=L6X2B4&q='
+
+    for merchant in merchantList:
+        resp = requests.get(uri + merchant).json()
+        f = open(merchant + '.json', 'w')
+        f.write(json.dumps(resp))
+        f.close()
+
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var merchants = fs.readFileSync('./dataset/merchants.txt', 'utf8').toString().split('\n');
+    const product = req.query.product.toLowerCase();
+    try {
+        output = [];
+        for (const merchant of merchants) {
+            if (merchant !== '') {
+                var data = fs.readFileSync('./dataset/' + merchant + '.json', 'utf8');
+                const obj = JSON.parse(data);
+                // check query
+                const items = obj.items;
+                for (const item of items) {
+                    if (item.name && item.name.toLowerCase().includes(product)) {
+                        output.push({"name": item.name, "price": item.current_price, "merchant": merchant, "merchant_id": item.merchant_id });
+                    } 
+                }
+            }
+        }
+        output.sort((a,b) => {
+            return a.price - b.price;
+        });
+        res.send(output);
+    } catch(e) {
+        console.log('Error: ', e.stack);
+    }  
+  
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
