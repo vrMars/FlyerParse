e4a5e24ba561e1660274659298e5c0cb2360c21a
Neelaksh Bhatia <vrmars98@gmail.com>
2020-04-26 20:29:30 -0400

    Force a product in query

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,32 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var merchants = fs.readFileSync('./dataset/merchants.txt', 'utf8').toString().split('\n');
-    const product = req.query.product.toLowerCase();
-    try {
-        output = [];
-        for (const merchant of merchants) {
-            if (merchant !== '') {
-                var data = fs.readFileSync('./dataset/' + merchant + '.json', 'utf8');
-                const obj = JSON.parse(data);
-                // check query
-                const items = obj.items;
-                for (const item of items) {
-                    if (item.name && item.name.toLowerCase().includes(product)) {
-                        output.push({"name": item.name, "price": item.current_price, "merchant": merchant, "merchant_id": item.merchant_id });
-                    } 
+    if (req.query.product) {
+        var merchants = fs.readFileSync('./dataset/merchants.txt', 'utf8').toString().split('\n');
+        const product = req.query.product.toLowerCase();
+        try {
+            output = [];
+            for (const merchant of merchants) {
+                if (merchant !== '') {
+                    var data = fs.readFileSync('./dataset/' + merchant + '.json', 'utf8');
+                    const obj = JSON.parse(data);
+                    // check query
+                    const items = obj.items;
+                    for (const item of items) {
+                        if (item.name && item.name.toLowerCase().includes(product)) {
+                            output.push({"name": item.name, "price": item.current_price, "merchant": merchant, "merchant_id": item.merchant_id });
+                        } 
+                    }
                 }
             }
-        }
-        output.sort((a,b) => {
-            return a.price - b.price;
-        });
-        res.send(output);
-    } catch(e) {
-        console.log('Error: ', e.stack);
+            output.sort((a,b) => {
+                return a.price - b.price;
+            });
+            res.send(output);
+        } catch(e) {
+            console.log('Error: ', e.stack);
+        }  
     }  
-  
   // res.render('index', { title: 'Express' });
 });
 
