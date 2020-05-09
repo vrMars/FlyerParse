0fc2e9eea7cbe9afe0649706f3aaf83ed9145e58
Neelaksh Bhatia <vrmars98@gmail.com>
2020-05-09 15:37:35 -0400

    Add support for array items

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,69 +3,120 @@ var router = express.Router();
 var fs = require('fs');
 const { spawn } = require('child_process');
 
+// router.post('/', (req, res, next) => {
+//     console.log('here');
+//     if (req.body.queryResult.parameters.food) {
+//         // run generation script
+//         const python = spawn('python3', ['./dataset/getFlyers.py']);
+//         python.on('close', (code) => {
+//             console.log(code);
+//             if (code == 0) {
+//                 var merchants = fs.readFileSync('./dataset/merchants.txt', 'utf8').toString().split('\n');
+//                 const product = req.body.queryResult.parameters.food.toLowerCase();
+//                 try {
+//                     output = [];
+//                     for (const merchant of merchants) {
+//                         if (merchant !== '') {
+//                             var data = fs.readFileSync('./dataset/' + merchant + '.json', 'utf8');
+//                             const obj = JSON.parse(data);
+//                             // check query
+//                             const items = obj.items;
+//                             for (const item of items) {
+//                                 if (item.name && item.name.toLowerCase().includes(product)) {
+//                                     output.push(
+//                                     {
+//                                         "name": item.name, 
+//                                         "price": item.current_price, 
+//                                         "merchant": item.merchant_name, 
+//                                         "merchant_id": item.merchant_id 
+//                                     });
+//                                 } 
+//                             }
+//                         }
+//                     }
+//                     output.sort((a,b) => {
+//                         return a.price - b.price;
+//                     });
+// 
+//                     const formattedOutput = 
+//                         {
+//                             "payload": {
+//                                 "google": {
+//                                     "expectUserResponse": true,
+//                                     "richResponse": {
+//                                         "items": [
+//                                             {
+//                                                 "simpleResponse": {
+//                                                     "textToSpeech": "You can find " + output[0].name + " at " + output[0].merchant + " for " + output[0].price + " dollars."
+//                                                 }
+//                                             }
+//                                         ]
+//                                     }
+//                                 }
+//                             }
+//                         };
+// 
+//                     res.send(formattedOutput);
+//                 } catch(e) {
+//                     console.log('Error: ', e.stack);
+//                 }  
+// 
+//             }
+// 
+//         })
+//     } else {
+//         res.render('index', { title: 'Express' });
+//     } 
+// });
+
 router.post('/', (req, res, next) => {
-    console.log('here');
-    if (req.body.queryResult.parameters.food) {
+    if (req.body.items) {
         // run generation script
         const python = spawn('python3', ['./dataset/getFlyers.py']);
         python.on('close', (code) => {
             console.log(code);
             if (code == 0) {
                 var merchants = fs.readFileSync('./dataset/merchants.txt', 'utf8').toString().split('\n');
-                const product = req.body.queryResult.parameters.food.toLowerCase();
+                const product = req.body.items.map(x => x.toLowerCase());
                 try {
-                    output = [];
-                    for (const merchant of merchants) {
-                        if (merchant !== '') {
-                            var data = fs.readFileSync('./dataset/' + merchant + '.json', 'utf8');
-                            const obj = JSON.parse(data);
-                            // check query
-                            const items = obj.items;
-                            for (const item of items) {
-                                if (item.name && item.name.toLowerCase().includes(product)) {
-                                    output.push(
-                                    {
-                                        "name": item.name, 
-                                        "price": item.current_price, 
-                                        "merchant": item.merchant_name, 
-                                        "merchant_id": item.merchant_id 
-                                    });
-                                } 
-                            }
-                        }
-                    }
-                    output.sort((a,b) => {
-                        return a.price - b.price;
-                    });
-
-                    const formattedOutput = 
-                        {
-                            "payload": {
-                                "google": {
-                                    "expectUserResponse": true,
-                                    "richResponse": {
-                                        "items": [
+                    const actualOutput = [];
+                    for (const p of products) {
+                        output = [];
+                        for (const merchant of merchants) {
+                            if (merchant !== '') {
+                                var data = fs.readFileSync('./dataset/' + merchant + '.json', 'utf8');
+                                const obj = JSON.parse(data);
+                                // check query
+                                const items = obj.items;
+                                for (const item of items) {
+                                    if (item.name && item.name.toLowerCase().includes(p)) {
+                                        output.push(
                                             {
-                                                "simpleResponse": {
-                                                    "textToSpeech": "You can find " + output[0].name + " at " + output[0].merchant + " for " + output[0].price + " dollars."
-                                                }
-                                            }
-                                        ]
-                                    }
+                                                "name": item.name, 
+                                                "price": item.current_price, 
+                                                "merchant": item.merchant_name, 
+                                                "merchant_id": item.merchant_id 
+                                            });
+                                    } 
                                 }
                             }
-                        };
+                        }
+                        output.sort((a,b) => {
+                            return a.price - b.price;
+                        });
+                        actualOutput.push({
+                            p: [...output]
+                        });
+                    }
 
-                    res.send(formattedOutput);
+                    res.send(actualOutput);
                 } catch(e) {
                     console.log('Error: ', e.stack);
                 }  
-
             }
-
-        })
+        });
     } else {
-        res.render('index', { title: 'Express' });
+        throw "ERROR";
     } 
 });
 
