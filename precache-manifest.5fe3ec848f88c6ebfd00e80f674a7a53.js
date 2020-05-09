23d8b0111772dedb9d2bec95283b769470ff3384
Neelaksh Bhatia <vrmars98@gmail.com>
2020-05-09 14:27:17 -0400

    Updates

---
+++ precache-manifest.5fe3ec848f88c6ebfd00e80f674a7a53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e32f54a0323f4ce5ce69bda91a991b",
+    "url": "/flyerParse/index.html"
+  },
+  {
+    "revision": "5f91d66c775b7cf94d2e",
+    "url": "/flyerParse/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61364d1ba9b30a293c28",
+    "url": "/flyerParse/static/js/2.ada1840a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flyerParse/static/js/2.ada1840a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f91d66c775b7cf94d2e",
+    "url": "/flyerParse/static/js/main.f3a2b233.chunk.js"
+  },
+  {
+    "revision": "91bbc518af21c4f3281e",
+    "url": "/flyerParse/static/js/runtime-main.75b6b886.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flyerParse/static/media/logo.5d5d9eef.svg"
+  }
+]);
